93f1d9324de5f3efd0f4953e9492ca4b69b98739
ChChCheeeeo <cheo.roman+github@gmail.com>
2015-05-13 18:10:10 -0700

    Changed base.py to include templates files folder

--- taskbuster_project/settings/base.py
+++ taskbuster_project/settings/base.py
@@ -75,14 +75,20 @@ ROOT_URLCONF = 'taskbuster_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, "templates")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
+                # default
                 'django.contrib.auth.context_processors.auth',
+                'django.template.context_processors.debug',
+                #'django.template.context_processors.request',
                 'django.contrib.messages.context_processors.messages',
+                # non default
+                'django.template.context_processors.i18n',
+                'django.template.context_processors.media',
+                'django.template.context_processors.static',
+                'django.template.context_processors.tz',
             ],
         },
     },
