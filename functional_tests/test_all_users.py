6e3cb079633a7ae5ad97b6dabf5678eb178e52c4
ChChCheeeeo <cheo.roman+github@gmail.com>
2015-05-13 20:35:06 -0700

    Changed functional_tests into module and test_all_users to test browser header and css property

---
+++ functional_tests/test_all_users.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+from django.contrib.staticfiles.testing import LiveServerTestCase
+from django.core.urlresolvers import reverse
+
+from selenium import webdriver
+import unittest
+
+class HomeNewVisitorTest(LiveServerTestCase):#unittest.TestCase):
+    """NewVisitorTest
+    """
+
+    # setUp and tearDown run before and after every
+    # test_
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+        self.browser.implicitly_wait(3)
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def get_full_url(self, namespace):
+        # gives local host url
+        # use this method because test server uses another url
+        # reverse gives relative url of a given namespace here /
+        # resulting function gives the absolute url of that 
+        # namespace (the sum of the previous two), 
+        return self.live_server_url + reverse(namespace)
+
+    def test_home_title(self):
+        self.browser.get(self.get_full_url("home"))
+        self.assertIn("TaskBuster", self.browser.title)
+
+    def test_h1_css(self):
+        # The css rule for the text color will be on a CSS 
+        # file, which means that if the test passes, 
+        # staticfiles are loading correctly.
+        self.browser.get(self.get_full_url("home"))
+        h1 = self.browser.find_element_by_tag_name("hi")
+        self.assertEqual(h1.value_of_css_property(
+            "color", 
+            "rgba(200, 50, 255, 1)"
+        )
+
