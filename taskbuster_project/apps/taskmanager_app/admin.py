a506425a4364630b6f43fb4cdc2704b8e9c7d535
ChChCheeeeo <cheo.roman+github@gmail.com>
2015-05-22 16:49:45 -0700

    Added custome list display and model inline to Project model

--- taskbuster_project/apps/taskmanager_app/admin.py
+++ taskbuster_project/apps/taskmanager_app/admin.py
@@ -5,20 +5,51 @@ from . import models
 
 # register ProfileAdmin as ModelAdmin of the Profile-Model
 # (where admin instance to the model to manage is connected)
-@admin.register(models.Profile)
-class ProfileAdmin(admin.ModelAdmin):
-    """ProfileAdmin
-    """
+# @admin.register(models.Profile)
+# class ProfileAdmin(admin.ModelAdmin):
+#    """ProfileAdmin
+#    """
     # defile ProfileAdmin as modelAdmin instance
 
     # define fields displayed when listing profile instances.
     # in admin, two columns seen: username, and interaction
     # since custom attribute usernamed defined, can use 
     # username instead of user__username
-    list_display = ("username", "interaction")
+#   list_display = ("username", "interaction")
 
     # create search box. field list specify which fields
     # are searched
     # for search field, need to use a normal model attribute
     # so use user__username instead of username.
+#    search_fields = ["user__username"]
+
+
+class ProjectsInLine(admin.TabularInline):
+    """ProjectsInLine
+    """
+    model = models.Project
+    # how many extra Projects should appear when editing a Profile insstanc.
+
+    extra = 0
+
+
+@admin.register(models.Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    """ProfileAdmin
+    """
+
+    list_display = ("username", "interaction", "_projects")
+
     search_fields = ["user__username"]
+
+    # Note that when creating a new project inside a profile instance, 
+    # the relation between both objects is automatically set 
+    # (don’t need to specify the profile field in the project instance).
+    # only Projects related to the current profile instance are shown 
+    # in the admin site..
+    inlines = [
+        ProjectsInLine
+    ]
+
+    def _projects(self, obj):
+        return obj.projects.all().count()
